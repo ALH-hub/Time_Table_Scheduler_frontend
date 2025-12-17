20862408a7b9d42473bf6cf219997a47356cbbd7
Alhadji Oumate <alhadjioum0@gmail.com>
2025-12-17 12:26:37 +0100

    chore: Remove AdminTimetableView component to simplify project structure

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,5 @@ export { default as Home } from './Home';
 export { default as Login } from './Login';
 export { default as StudentPortal } from './StudentPortal';
 export { default as AdminDashboard } from './AdminDashboard';
-export { default as AdminTimetableView } from './AdminTimetableView';
 export { default as NotFound } from './NotFound';
 export { default as Loading } from './Loading';
